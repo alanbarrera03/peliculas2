5af684802988c893b122782e466b84efb45b4d3e
Alan Barrera <aalan.eduardo@gmail.com>
2021-12-27 20:45:57 -0800

    27dic

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/search/search_delegate.dart';
+import 'package:peliculas/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+import 'package:peliculas/providers/movies_provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('Peliculas en cines')),
+          elevation: 0,
+          actions: [
+            IconButton(
+                onPressed: () => showSearch(
+                    context: context, delegate: MovieSearchDelegate()),
+                icon: const Icon(Icons.search_outlined))
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              CardSwiper(
+                movies: moviesProvider.onDisplayMovies,
+              ),
+              MovieSlider(
+                movies: moviesProvider.popularMovies,
+                title: 'Populares',
+                onNextPage: () => moviesProvider.getPopularMovies(),
+              )
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/widgets/casting_cards.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:peliculas/models/models.dart';
+import 'package:peliculas/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
+
+class CastingCards extends StatelessWidget {
+  final int movieId;
+
+  CastingCards(this.movieId);
+
+  @override
+  Widget build(BuildContext context) {
+    final movieProvider = Provider.of<MoviesProvider>(context, listen: false);
+
+    return FutureBuilder(
+        future: movieProvider.getMovieCast(movieId),
+        builder: (BuildContext context, AsyncSnapshot<List<Cast>> snapshot) {
+          if (!snapshot.hasData) {
+            return Container(
+              constraints: const BoxConstraints(maxWidth: 150),
+              height: 180,
+              child: const CupertinoActivityIndicator(),
+            );
+          }
+
+          final List<Cast> cast = snapshot.data!;
+          return Container(
+            margin: const EdgeInsets.only(bottom: 30),
+            width: double.infinity,
+            height: 180,
+            child: ListView.builder(
+                itemCount: 10,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, index) =>
+                    _CastCard(cast[index])),
+          );
+        });
+  }
+}
+
+class _CastCard extends StatelessWidget {
+  final Cast actor;
+
+  const _CastCard(this.actor);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      width: 110,
+      height: 100,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+                placeholder: const AssetImage('assets/no-image.jpg'),
+                image: NetworkImage(actor.fullprofilePath)),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            actor.name,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    );
+  }
+}
